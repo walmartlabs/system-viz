8bdd8e2d2703d945d59fcc94693ce1ca6c680969
Howard M. Lewis Ship <hlship@walmartlabs.com>
2015-12-10 09:40:42 -0800

    Copy in the main code with improvements

--- src/com/walmartlabs/system_viz.clj
+++ src/com/walmartlabs/system_viz.clj
@@ -1,1 +1,97 @@
-(ns com.walmartlabs.system-viz)
+(ns com.walmartlabs.system-viz
+  "Visualize a component system using Graphviz."
+  (:require [com.stuartsierra.component :as component]
+            [io.aviso.toolchest.macros :refer [cond-let]]
+            [clojure.string :as str]
+            [clojure.java.browse :refer [browse-url]])
+  (:import (java.io File)
+           (java.util.concurrent TimeUnit)))
+
+(defn- simplify
+  "Removes characters that outside of alphanumerics."
+  [s]
+  (str/replace s #"\W" ""))
+
+(defn- system->dot
+  [system]
+  (let [component-ids (keys system)
+        component-id->node-id (zipmap component-ids
+                                      (map #(-> % name simplify gensym) component-ids))]
+    (println "digraph System {")
+
+    (doseq [[k v] component-id->node-id]
+      (println (format "  %s [label=\"%s\"];" v k))
+      (doseq [[local-id system-id] (-> system
+                                       (get k)
+                                       component/dependencies)]
+        (print (format "  %s -> %s"
+                       v
+                       (component-id->node-id system-id)))
+
+        (when-not (= local-id system-id)
+          (print (format " [label=\"%s\"]"
+                         local-id)))
+
+        (println ";")))
+
+    (println "}")))
+
+(def ^:private wait 5)
+
+(defn visualize-system
+  "Visualizes the system as a graph, using Graphviz.
+
+  Options:
+
+  enabled
+  : if true, then the graph will be generated and opened.
+    Defaults to false, since this is generally unwanted behavior
+    in production.
+
+  open
+  : if true (the default), then the generated image file
+    will be opened.  If false, then the path to the image file
+    will be printed to *out*.
+
+  Returns the system unchanged."
+  [system options]
+
+  (cond-let
+  [{:keys [enabled open]
+      :or {enabled false
+           open true}} options]
+
+    (not enabled)
+    nil
+
+    [dot (with-out-str
+           (system->dot system))
+     gvfile (File/createTempFile "system-" ".gv")
+     imagefile (File/createTempFile "system-" ".pdf")
+     process (do
+               (spit gvfile dot)
+               (.exec (Runtime/getRuntime) (str "dot -Tpdf " gvfile " -o " imagefile)))]
+    (do
+      (.waitFor process wait (TimeUnit/SECONDS))
+      (.isAlive process))
+    (binding [*out* *err*]
+      (println "DOT process (to render system map) did not finish after" wait "seconds.")
+      (.destroyForcibly process))
+
+    [exit-value (.exitValue process)]
+
+    (not (zero? exit-value))
+    (binding [*out* *err*]
+      (println "DOT process failed with status" (.exitValue process))
+      (println (slurp (.getErrorStream process))))
+
+    [image-url (.toURL imagefile)]
+
+    open
+    (browse-url image-url)
+
+    :else
+    (println "System graph rendered to:" (str image-url)))
+
+  ;; Ignore the result of cond-let, and just return the system.
+  system)
---
+++ test/com/walmartlabs/system_viz_test.clj
@@ -0,0 +1,15 @@
+(ns com.walmartlabs.system-viz-test
+  (:require [com.walmartlabs.system-viz :refer [visualize-system]]
+            [com.stuartsierra.component :as component]))
+
+(def sys
+  (component/system-map
+    :auth (component/using {} {:delegate :local/auth})
+    :local/auth (component/using {} [:database])
+    :database (component/using {} [])
+    :handler (component/using {} [:database :message-queue])
+    :message-queue {}
+    :router (component/using {} {:queue :message-queue})
+    :web-server (component/using {} [:auth :router :handler])))
+
+(def options {:enabled true})
